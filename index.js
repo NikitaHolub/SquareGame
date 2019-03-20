9a103cfa32c2373843b356f586efb30aeacda047
Nik <nikitaholub1999@gmail.com>
2019-03-20 11:33:33 +0200

    end game, score, set time

--- index.js
+++ index.js
@@ -3,22 +3,78 @@
 //get nodes from document
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
+let $timeHeader = document.querySelector('#time-header');
+let $resultHeader = document.querySelector('#result-header');
+let $result = document.querySelector('#result');
+let $gameTime = document.querySelector('#game-time');
 
 let score = 0;
+let isGameStarted = false;
 
+function show($el) {
+  $el.classList.remove('hide');
+}
+
+function hide($el) {
+  $el.classList.add('hide');
+}
 //game events
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
 
 function startGame() {
+  score = 0;
+  setGameTime();
+  $gameTime.setAttribute('disabled', 'true');
+  isGameStarted = true;
   $game.style.backgroundColor = '#fff'; 
-  $start.classList.add('hide'); 
+  hide($start); 
+
+  let interval = setInterval(function() {
+    let time = parseFloat($time.textContent);
+
+    if (time <= 0) {
+      clearInterval(interval);
+      endGame();
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+    console.log('interval', $time.textContent);
+  }, 100)
 
   renderBox();
 }
 
+function setGameScore() {
+  $result.textContent = score.toString();
+}
+
+function setGameTime() {
+  let time = +$gameTime.value;
+  $time.textContent = time.toFixed(1);
+  show($timeHeader);
+  hide($resultHeader); 
+}
+
+function endGame() {
+  isGameStarted = false;
+  $gameTime.removeAttribute('disabled');
+  setGameScore();
+  show($start);
+  $game.innerHTML = '';
+  $game.style.backgroundColor = '#ccc';
+  hide($timeHeader);
+  show($resultHeader);
+}
+
 function handleBoxClick(event) {
+  if (!isGameStarted) {
+    return;
+  }
+
   if (event.target.dataset.box) {
     score++;
     renderBox();
@@ -35,6 +91,7 @@ function renderBox() {
   let maxTop = gameSize.height - boxSize;
   let maxLeft = gameSize.width - boxSize;
   let boxColor = [];
+
   for (let i = 0; i < 3; i++) {
     boxColor[i] = getRandom(0, 256);
   }
